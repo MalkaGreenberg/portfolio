e4ddc9aa2c5ad5068aae0d605d15cd9e7367110f
MalkaGreenberg <malkagreenberg9@gmail.com>
2023-11-23 16:57:44 -0500

    removed unused varialble

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import AboutMe from './pages/AboutMe';
 import Portfolio from './pages/Portfolio';
