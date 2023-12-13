80118bafca199221dfccdce81881ca8f5c602038
MalkaGreenberg <malkagreenberg9@gmail.com>
2023-12-12 21:13:57 -0500

    added reel delights to portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -7,21 +7,22 @@ import quiz from '../images/Quiz.png';
 import AGM from '../images/agm.png';
 import weather from '../images/screenshot.png';
 import password from '../images/password.png';
+import reelDelights from '../images/reelDelights.png';
 
 const Portfolio = () => {
   // Create an array of project data
   const projects = [
     {
+      title: 'Reel Delights',
+      image: reelDelights,
+      deployedLink: 'https://reel-delights.onrender.com/',
+      githubLink: 'https://github.com/MalkaGreenberg/reel-delights/',
+    },
+    {
       title: 'Discover Skin',
       image: skincare,
       deployedLink: 'https://discover-skin-7a59e0f720d4.herokuapp.com/homepage',
       githubLink: 'https://github.com/Clarisagonzalez/Discover-Skin',
-    },
-    {
-      title: 'Coding Quiz',
-      image: quiz,
-      deployedLink: 'https://malkagreenberg.github.io/JavaScript-Quiz/',
-      githubLink: 'https://github.com/MalkaGreenberg/JavaScript-Quiz',
     },
     {
       title: 'Note Taker',
@@ -46,6 +47,12 @@ const Portfolio = () => {
       image: password,
       deployedLink: 'https://malkagreenberg.github.io/Password-Generator',
       githubLink: 'https://github.com/MalkaGreenberg/Password-Generator',
+    },
+    {
+      title: 'Coding Quiz',
+      image: quiz,
+      deployedLink: 'https://malkagreenberg.github.io/JavaScript-Quiz/',
+      githubLink: 'https://github.com/MalkaGreenberg/JavaScript-Quiz',
     },
     // Add more projects as needed
   ];
